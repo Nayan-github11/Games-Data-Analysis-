77a1af3fa2c9f936e69099a639d4bf326f01c2c8
Nayan Adangale <122141219+Nayan-github11@users.noreply.github.com>
2024-06-10 19:19:06 +0530

    Add files via upload

---
+++ Project_2.sql
@@ -0,0 +1,10 @@
+show databases;
+use gs_data;
+show tables;
+
+select * from game_sales_data;
+desc game_sales_data;
+select * from vgsales;
+drop table vgsales;
+
+select * from game_sales_data where year = 2020;
